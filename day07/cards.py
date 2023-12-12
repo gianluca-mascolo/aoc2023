8083b149fecda51f8d8c2d8a6294cf6af877fdec
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-12-12 10:39:10 +0100

    hand kind

--- day07/cards.py
+++ day07/cards.py
@@ -20,7 +20,33 @@ class Card:
 
 class Hand:
     def __init__(self, cards: list[Card]):
+        assert len(cards)==5
         self.cards = [c for c in cards]
+    def kind(self):
+        card_groups = {}
+        for card in self.cards:
+            if card.value in card_groups:
+                card_groups[card.value]+=1
+            else:
+                card_groups[card.value]=1
+        if len(card_groups)==1:
+            print("Five of a kind")
+        elif len(card_groups)==2:
+            if max(card_groups.values())==4:
+                print("Four of a kind")
+            else:
+                print("Full House")
+        elif len(card_groups)==3:
+            if max(card_groups.values())==3:
+                print("Three of a kind")
+            else:
+                print("Two pairs")
+        elif len(card_groups)==4:
+            print("One Pair")
+        elif len(card_groups)==5:
+            print("High Card")
+
+        return card_groups
 
 def main():
     with open('example', 'r') as reader:
@@ -31,10 +57,10 @@ def main():
             line = reader.readline()
 
     c1 = Card("A")
-    c2 = Card("2")
-    c3 = Card("Q")
-    c4 = Card("T")
-    c5 = Card("3")
+    c2 = Card("Q")
+    c3 = Card("8")
+    c4 = Card("9")
+    c5 = Card("T")
     print(c1<c2)
     print(c3!=c1)
     print(c1==c4)
@@ -42,5 +68,7 @@ def main():
     print([ c.value for c in hand.cards])
     hand.cards.sort()
     print([ c.value for c in hand.cards])
+    print(hand.kind())
+    print(max(hand.kind().values()))
 if __name__ == "__main__":
     main()
