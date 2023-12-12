cc569217fc87458d398773b00917a3922540593d
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-12-12 10:00:21 +0100

    hand object

--- day07/cards.py
+++ day07/cards.py
@@ -18,6 +18,10 @@ class Card:
     def __ge__(self, other):
         return DECK.index(self.value)>=DECK.index(other.value)
 
+class Hand:
+    def __init__(self, cards: list[Card]):
+        self.cards = [c for c in cards]
+
 def main():
     with open('example', 'r') as reader:
         line = reader.readline()
@@ -26,12 +30,17 @@ def main():
             print(line)
             line = reader.readline()
 
-    c1 = Card("T")
+    c1 = Card("A")
     c2 = Card("2")
     c3 = Card("Q")
     c4 = Card("T")
+    c5 = Card("3")
     print(c1<c2)
     print(c3!=c1)
     print(c1==c4)
+    hand = Hand([c1,c2,c3,c4,c5])
+    print([ c.value for c in hand.cards])
+    hand.cards.sort()
+    print([ c.value for c in hand.cards])
 if __name__ == "__main__":
     main()
