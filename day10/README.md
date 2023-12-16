2f1fbadd1da5c88b8468bbacf028bcb617829b8d
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-12-16 01:07:22 +0100

    day10 part 1 readme

---
+++ day10/README.md
@@ -0,0 +1,4 @@
+execute with
+labyrint.py < input
+
+it will print the labyrint in ASCII art without unused pipes and print the total steps of the loop.
--- day10/labyrinth.py
+++ day10/labyrinth.py
@@ -1,75 +1,108 @@
 #!/usr/bin/env python3
 import sys
 
-SYMBOLS = {"-": "\u2550", "|": "\u2551", "F": "\u2554", "7": "\u2557", "L": "\u255A", "J": "\u255D", "S": "*", ".": "."}
+SYMBOLS = {"-": "\u2550", "|": "\u2551", "F": "\u2554", "7": "\u2557", "L": "\u255A", "J": "\u255D", "S": "*", ".": ".", "X": " "}
+PIPES = {"-": {"E", "W"}, "|": {"N", "S"}, "F": {"S", "E"}, "7": {"S", "W"}, "L": {"N", "E"}, "J": {"N", "W"}}
+CONNECTIONS = {"E": "W", "W": "E", "N": "S", "S": "N"}
+WALK = {"E": (1, 0), "W": (-1, 0), "N": (0, -1), "S": (0, 1)}
 
-PIPES = {"-": {"east", "west"}, "|": {"north", "south"}, "F": {"south", "east"}, "7": {"south", "west"}, "L": {"north", "east"}, "J": {"north", "west"}}
 
-CONNECTIONS = {"east": "west", "west": "east", "north": "south", "south": "north"}
+class Nodo:
+    def __init__(self, x: int, y: int, pipe: str):
+        self.coord = {"x": x, "y": y}
+        self.pipe = pipe
+        self.symbol = SYMBOLS[pipe]
+        self.connect = PIPES[pipe] if pipe in PIPES else {}
 
 
-def printmap(lab: list):
-    for line in lab:
-        for c in line:
-            print(SYMBOLS[c], end="")
+class LabMap:
+    def __init__(self):
+        self.map = []
+        self.sizex = -1
+        self.sizey = -1
+
+    def print(self):
+        y = 0
+        for node in self.map:
+            if node.coord["y"] > y:
+                y += 1
+                print("")
+            print(node.symbol, end="")
         print("")
 
+    def get(self, x: int, y: int):
+        for node in self.map:
+            if node.coord == {"x": x, "y": y}:
+                return node
+        return None
 
-def findstart(lab: list):
-    for y, line in enumerate(lab):
-        for x, c in enumerate(line):
-            if c == "S":
-                return (y, x)
+    def update(self, x: int, y: int, pipe: str):
+        for node in self.map:
+            if node.coord == {"x": x, "y": y}:
+                node.pipe = pipe
+                node.connect = PIPES[pipe] if pipe in PIPES else {}
+                node.symbol = SYMBOLS[pipe]
+
+    def look(self, x: int, y: int, direction: str):
+        if direction == "N" and y > 0:
+            return self.get(x, y - 1).connect
+        elif direction == "S" and y < self.sizey - 1:
+            return self.get(x, y + 1).connect
+        elif direction == "W" and x > 0:
+            return self.get(x - 1, y).connect
+        elif direction == "E" and x < self.sizex - 1:
+            return self.get(x + 1, y).connect
+        else:
+            return {}
+
+    def findstart(self):
+        start_node = next(node for node in self.map if node.pipe == "S")
+        sx = start_node.coord["x"]
+        sy = start_node.coord["y"]
+        for pipe in PIPES:
+            self.update(sx, sy, pipe)
+            total_connections = 0
+            for connection in self.get(sx, sy).connect:
+                if CONNECTIONS[connection] in self.look(sx, sy, connection):
+                    total_connections += 1
+            if total_connections == 2:
+                break
+        return start_node
 
 
 def main():
-    labyrinth_map = []
+    labyrinth = LabMap()
+    y = 0
     while line := sys.stdin.readline():
         line = line.rstrip()
-        labyrinth_map.append([c for c in line if c in SYMBOLS])
-    y, x = findstart(labyrinth_map)
-    for start in PIPES:
-        labyrinth_map[y][x] = start
-        num_connections = 0
-        for conn in PIPES[start]:
-            if x + 1 < len(labyrinth_map) and labyrinth_map[y][x + 1] in PIPES and conn == "east" and "west" in PIPES[labyrinth_map[y][x + 1]]:
-                num_connections += 1
-            if y + 1 < len(labyrinth_map) and labyrinth_map[y + 1][x] in PIPES and conn == "south" and "north" in PIPES[labyrinth_map[y + 1][x]]:
-                num_connections += 1
-            if x - 1 > 0 and labyrinth_map[y][x - 1] in PIPES and conn == "west" and "east" in PIPES[labyrinth_map[y][x - 1]]:
-                num_connections += 1
-            if y - 1 > 0 and labyrinth_map[y - 1][x] in PIPES and conn == "north" and "south" in PIPES[labyrinth_map[y - 1][x]]:
-                num_connections += 1
-        if num_connections == 2:
-            break
-    printmap(labyrinth_map)
-    print(x, y)
-    walk_to = list(PIPES[labyrinth_map[y][x]])[0]
+        for x, c in enumerate([s for s in line if s in SYMBOLS]):
+            node = Nodo(x, y, c)
+            labyrinth.map.append(node)
+        y += 1
+    labyrinth.sizey = y
+    labyrinth.sizex = x+1
+    start_node = labyrinth.findstart()
+    sx = start_node.coord["x"]
+    sy = start_node.coord["y"]
+    walk_to = next(iter(start_node.connect))
+    x = sx
+    y = sy
     steps = 0
-    nx = x
-    ny = y
+    loop_nodes = []
     while True:
-        print(f"[{steps}] {walk_to} -> ", end="")
-        if walk_to == "east":
-            dx = 1
-            dy = 0
-        elif walk_to == "north":
-            dy = -1
-            dx = 0
-        elif walk_to == "south":
-            dx = 0
-            dy = 1
-        elif walk_to == "west":
-            dx = -1
-            dy = 0
-        nx += dx
-        ny += dy
-        walk_to = list(PIPES[labyrinth_map[ny][nx]] - {CONNECTIONS[walk_to]})[0]
-        print(f"{walk_to} {nx} {ny} ({x} {y})")
-        if nx == x and ny == y:
+        loop_nodes.append({"x": x, "y": y})
+        dx, dy = WALK[walk_to]
+        walk_to = list(labyrinth.look(x, y, walk_to) - {CONNECTIONS[walk_to]})[0]
+        x += dx
+        y += dy
+        if x == sx and y == sy:
             break
         steps += 1
-    print(int((steps + 1) / 2))
+    for node in labyrinth.map:
+        if node.coord not in loop_nodes and node.pipe in PIPES:
+            labyrinth.update(node.coord["x"], node.coord["y"], "X")
+    labyrinth.print()
+    print(f"Loop steps: {steps}")
 
 
 if __name__ == "__main__":
