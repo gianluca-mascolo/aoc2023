1469e8bb49d934bdd54fde80273ac7b89c91e9e4
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2023-12-27 14:55:54 +0100

    day16 p1 ok

--- day16/mirrors.py
+++ day16/mirrors.py
@@ -54,10 +54,11 @@ class Tile:
 
 
 class TileMap:
-    def __init__(self):
+    def __init__(self, startbeam: Beam):
         self.map = []
         self.rows = 0
         self.cols = 0
+        self.startbeam = startbeam
 
     def print(self):
         for row in self.map:
@@ -65,56 +66,56 @@ class TileMap:
 
     def printenergy(self):
         for row in self.map:
-            print("".join([('.','#')[tile.energized] for tile in row]))
+            print("".join([(".", "#")[tile.energized] for tile in row]))
 
     def xray(self):
-        for ray,path in enumerate(self.beam):
+        for ray, path in enumerate(self.beam):
             print([(ray, b.x, b.y, b.speed, b.direction) for b in path])
 
     def __iter__(self):
-        self.beam = [[Beam(0, 0, 1, "R")]]
+        self.beam = [[self.startbeam]]
         self.seen = set()
         return self
 
     def __next__(self):
-        visited = set()
+        touch = set()
         last_ray = []
         for path in self.beam:
             last_ray.append(path[-1])
-        for ray,last in enumerate(last_ray):
+        for ray, last in enumerate(last_ray):
             if last.speed > 0:
-                visited.add((last.x,last.y))
+                touch.add((last.x, last.y))
                 self.map[last.y][last.x].energized = True
                 delta = self.map[last.y][last.x].reflect(last.direction)
             else:
                 delta = set()
-            if len(delta)==1:
+            if len(delta) == 1:
                 newdir = delta.pop()
                 dx, dy = MOVE[newdir]
-                if last.x + dx >= 0 and last.x + dx < self.cols and last.y + dy >= 0 and last.y+dy < self.rows and (last.x,last.y,newdir) not in self.seen:
-                    self.beam[ray].append(Beam(last.x+dx,last.y+dy,1,newdir))
-                    self.seen.add((last.x,last.y,newdir))
+                if last.x + dx >= 0 and last.x + dx < self.cols and last.y + dy >= 0 and last.y + dy < self.rows and (last.x + dx, last.y + dy, newdir) not in self.seen:
+                    self.beam[ray].append(Beam(last.x + dx, last.y + dy, 1, newdir))
+                    self.seen.add((last.x + dx, last.y + dy, newdir))
                 else:
-                    self.beam[ray][-1]=Beam(last.x,last.y,0,last.direction)
-            elif len(delta)==2:
+                    self.beam[ray][-1] = Beam(last.x, last.y, 0, last.direction)
+            elif len(delta) == 2:
                 self.beam.append(self.beam[ray].copy())
                 for r in [ray, -1]:
                     newdir = delta.pop()
                     dx, dy = MOVE[newdir]
-                    if last.x + dx >= 0 and last.x + dx < self.cols and last.y + dy >= 0 and last.y+dy < self.rows and (last.x,last.y,newdir) not in self.seen:
-                        self.beam[r].append(Beam(last.x+dx,last.y+dy,1,newdir))
-                        self.seen.add((last.x,last.y,newdir))
+                    if last.x + dx >= 0 and last.x + dx < self.cols and last.y + dy >= 0 and last.y + dy < self.rows and (last.x + dx, last.y + dy, newdir) not in self.seen:
+                        self.beam[r].append(Beam(last.x + dx, last.y + dy, 1, newdir))
+                        self.seen.add((last.x + dx, last.y + dy, newdir))
                     else:
-                        self.beam[r][-1]=Beam(last.x,last.y,0,last.direction)
+                        self.beam[r][-1] = Beam(last.x, last.y, 0, last.direction)
         if all(r[-1].speed == 0 for r in self.beam):
             raise StopIteration
         else:
-            return visited
+            return touch
 
 
 def main():
-    grid = iter(TileMap())
-    with open("input", "r") as reader:
+    grid = iter(TileMap(Beam(0, 0, 1, "R")))
+    with open("example", "r") as reader:
         y = 0
         line = reader.readline()
         while line != "":  # The EOF char is an empty string
@@ -128,26 +129,21 @@ def main():
             y += 1
     grid.rows = y
     grid.cols = x + 1
+    print("Original map\n")
     grid.print()
-    #grid.xray()
-    n=0
+    print("")
     for step in grid:
-        if n > 10000:
-            break
-        print(f"step {n} {len(grid.beam)}")
-        n+=1
-        
-    #grid.xray()
+        pass
 
+    print("Energized map\n")
     grid.printenergy()
+    print("")
     total = 0
     for row in grid.map:
         for tile in row:
             if tile.energized:
                 total += 1
-    print(total)
-    print(grid.seen)
-    print(len(grid.seen))
+    print(f"Energized tiles:  {total}")
 
 
 if __name__ == "__main__":
